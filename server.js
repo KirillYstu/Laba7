f7b7513f33827880dfc340c23ee7f4b506ce50e7
KirillYstu <73735818+KirillYstu@users.noreply.github.com>
2021-01-17 19:43:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ var app = express();
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.get('/', function(req,res){ 
-    res.sendFile(__dirname + "/проект/student.html");
+    res.sendFile(__dirname + "/student.html");
 });
 
 app.post('/', urlencodedParser, function(req,res){ 
     if (!req.body) return res.status(400);
     console.log(req.body);
-    res.sendFile(__dirname + "/проект/student.html");; 
+    res.sendFile(__dirname + "/student.html");; 
 });
 
 app.listen(3000);
